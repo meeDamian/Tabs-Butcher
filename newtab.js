fc582ff39fa7abb9e7a9e6cbd094c85f65d3477c
Damian Mee <spam@meedamian.com>
2020-04-25 16:17:14 +0800

    time & bkg

--- newtab.js
+++ newtab.js
@@ -1,92 +1,107 @@
 'use strict';
 
-const GIPHY_API_KEY = 'G6LJJDUAk2KaGiBAP1u280ypodWCk8iu';
-
-const modes = ['diff', 'total', 'split'];
-const views = ['single', 'dual', 'random'];
-
-const titles = {
-	change: 'Change in the number of tabs during current session',
-	total: 'Total amount of tabs currently opened'
-};
+const modes = ['diff', 'total', 'split', 'fuck'];
 
 const {local} = chrome.storage;
 
-const style = (id, prop, value) => document.getElementById(id).style[prop] = value;
-const showView = view => views.map(v => style(v, 'opacity', v === view ? '1' : '0'));
-const fmtDate = ts => new Date(ts).toISOString().replace(/[TZ]/g, ' ').trim();
+const style = (id, propName, value) => document.getElementById(id).style[propName] = value;
+const html = (selector, value, propName = 'innerHTML') => document.querySelector(selector)[propName] = value;
+const counter = (id, value) => html(`#${id} h1`, value);
+
+function fmtDate(ts, ago = true) {
+	if (!ago) {
+		return new Date(ts).toLocaleString('sv');
+	}
+
+	const UNITS = ['second', 'minute', 'hour', 'day', 'week', 'month', 'year'];
+	const SEC_ARRAY = [ 60, 60, 24, 7, 365 / 7 / 12, 12];
+
+	let diff = (+new Date - ts) / 1000;
+
+	let idx = 0;
+
+	for (; diff >= SEC_ARRAY[idx] && idx < SEC_ARRAY.length; idx++) {
+		diff /= SEC_ARRAY[idx];
+	}
+
+	diff = Math.floor(diff);
+
+	idx *= 2;
+
+	if (diff > (idx === 0 ? 9 : 1)) idx += 1;
+
+	if (idx === 0) return 'just now';
+	let unit = UNITS[Math.floor(idx / 2)];
+	if (diff > 1) unit += 's';
+	return `${diff} ${unit} ago`
+}
 
 function since(start) {
 	if (!start) return;
 
-	document.querySelector('#since').innerHTML = fmtDate(start);
+	html('#since', fmtDate(start));
+	html('#since', fmtDate(start, false), 'title');
 	style('session-start', 'display', 'block');
 }
 
-function single(label, number, start) {
-	document.querySelector('#single h1').innerHTML = number;
-	document.querySelector('#single h2').innerHTML = label;
-
-	document.getElementById('single').title = titles[label];
-	since(start);
-
-	showView('single');
-}
-
-function dual(opened, closed, start) {
-	document.querySelector('#opened h1').innerHTML = opened;
-	document.querySelector('#closed h1').innerHTML = closed;
-	since(start);
-
-	showView('dual');
-}
-
 function fuck() {
+	const GIPHY_API_KEY = 'G6LJJDUAk2KaGiBAP1u280ypodWCk8iu';
 	const queries = ['zoomies', 'squirrel', 'dogs', 'puppies'];
 
-	fetch(`http://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}&tag=${queries[Math.floor(Math.random() * queries.length)]}`)
+	const randomQuery = queries[Math.floor(Math.random() * queries.length)];
+
+	fetch(`http://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}&tag=${randomQuery}`)
 		.then(res => res.json())
 		.then(({data: {image_url, title}}) => {
-			const img = document.querySelector('#random img');
+			const img = document.querySelector('#fuck-view img');
 			img.setAttribute('src', image_url);
 			img.setAttribute('title', title);
-			showView('random');
+			img.setAttribute('alt', title);
 		});
+}
+
+function populate({mode, last, start}) {
+	if (mode === 'fuck') return fuck();
+	if (mode === 'total') return counter(`${mode}-view`, last.total);
+
+	since(start);
+
+	if (mode === 'diff') {
+		const id = `${mode}-view`;
+		const {delta} = last;
+
+		counter(id, delta);
+
+		const {classList} = document.getElementById(id);
+		classList.remove('good', 'bad');
+
+		if (delta > 10) classList.add('bad');
+		if (delta < -10) classList.add('good');
+	}
+
+	if (mode === 'split') {
+		counter('opened', last.opened);
+		counter('closed', last.closed);
+	}
 }
 
 function refresh() {
 	style('session-start', 'display', 'none');
 
 	local.get(['mode', 'last', 'start'], ({mode, last, start}) => {
-		console.log('ddd', mode);
+		populate({mode, last, start});
 
-		switch (mode) {
-			case 'diff' : single('change', last.delta, start); break;
-			case 'total': single('total', last.total);         break;
-			case 'split': dual(last.opened, last.closed, start);     break;
-			case 'fuck' : fuck(); break;
-		}
-
-		for (const href of document.querySelectorAll('#modes a')) {
-			href.style.fontWeight = 'normal';
-		}
-
-		document.getElementById(mode).style.fontWeight = 'bold';
+		modes.map(m => {
+			style(m, 'fontWeight', m === mode ? 'bold' : 'normal');
+			style(`${m}-view`, 'opacity', m === mode ? '1' : '0');
+		});
 	});
 }
 
-function setMode(mode) {
-	local.set({mode});
-	refresh();
-}
-
-// Restore `mode` from storage, and set it for the current tab
-local.get('mode', ({mode = modes[0]}) => setMode(mode));
-
-// Register click-listeners on all mode-changing `<a href`s
-for (const mode of [...modes, 'fuck']) {
+// Register mode-changing click-listeners on footer anchors
+for (const mode of modes) {
 	document.getElementById(mode).onclick = () => {
-		setMode(mode);
+		local.set({mode}, refresh);
 		return false;
 	};
 }
@@ -102,6 +117,8 @@ getCurrent(({id}) => {
 });
 
 // Update counters in current tab if other tabs open/close
-const delayedRefresh = () => setTimeout(refresh, 1e3);
+const delayedRefresh = () => setTimeout(refresh, 500);
 onCreated.addListener(delayedRefresh);
 onRemoved.addListener(delayedRefresh);
+
+refresh();
